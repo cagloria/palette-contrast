f9466ebd80e7ac1f2becaba359a0c8c231e6a2fa
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-03 13:18:48 -0700

    Added layout styling to graphic panel

--- src/components/GraphicContrast.js
+++ src/components/GraphicContrast.js
@@ -2,25 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Panel1 = styled.div`
-    width: 100px;
-    height: 100px;
     border-radius: 8px 0 0 8px;
     background-color: ${(props) => props.color};
 `;
 
 const Panel2 = styled.div`
-    width: 100px;
-    height: 100px;
     border-radius: 0 8px 8px 0;
     background-color: ${(props) => props.color};
 `;
 
+const GraphicContainer = styled.div`
+    height: 100px;
+    width: 200px;
+    display: flex;
+    > div {
+        width: 50%;
+    }
+`;
+
 function GraphicContrast({ color1, color2 }) {
     return (
-        <div className="graphic-panel">
+        <GraphicContainer>
             <Panel1 color={color1} />
             <Panel2 color={color2} />
-        </div>
+        </GraphicContainer>
     );
 }
 
