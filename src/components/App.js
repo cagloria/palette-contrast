8424ba15a93bb469cb54599a3f472617ef590116
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-02 17:37:25 -0700

    Organized files

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import "../css/App.css";
+
+function App() {
+    const [primary, setPrimary] = useState("#CCCCCC");
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
