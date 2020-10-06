6209380e4be5b7d75a4a2c509f76148b937afb9b
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-05 17:01:12 -0700

    Set accent scheme to update on changing primary

--- src/components/AccentColorScheme.js
+++ src/components/AccentColorScheme.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import tinycolor from "tinycolor2";
 
 const Color = styled.button`
     width: 50px;
@@ -13,10 +14,36 @@ const SchemeContainer = styled.div`
     display: flex;
 `;
 
-function AccentColorScheme({ name, colors }) {
+const schemeSelect = {
+    Analogous: {
+        getScheme(primary) {
+            const colorObj = tinycolor(primary).analogous();
+            let hexCodes = [];
+
+            colorObj.forEach((obj) => {
+                const color = obj.toHexString();
+                if (!hexCodes.includes(color)) {
+                    hexCodes.push(color);
+                }
+            });
+
+            return hexCodes;
+        },
+    },
+};
+
+function AccentColorScheme({ primary, scheme }) {
+    const [colors, setColors] = useState(
+        schemeSelect[scheme].getScheme(primary)
+    );
+
+    useEffect(() => {
+        setColors(schemeSelect[scheme].getScheme(primary));
+    }, [primary, scheme]);
+
     return (
         <div>
-            <h3>{name}</h3>
+            <h3>{scheme}</h3>
             <SchemeContainer>
                 {colors.map((color) => (
                     <Color color={color} key={color}></Color>
--- src/components/AccentSelect.js
+++ src/components/AccentSelect.js
@@ -1,38 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AccentColorScheme from "./AccentColorScheme";
-import tinycolor from "tinycolor2";
 
 function AccentSelect({ primary, color, onAccentChange }) {
     const [value, setValue] = useState(color);
-    const [analogous, setAnalogous] = useState([]);
-
-    useEffect(() => {
-        if (analogous.length <= 0) {
-            getAnalogousColors();
-        }
-    });
 
     function handleAccentChange(event) {
         setValue(event.target.value);
         onAccentChange(event.target.value);
     }
 
-    function getAnalogousColors() {
-        const options = tinycolor(primary).analogous();
-        let hexCodes = [];
-        options.forEach((obj) => {
-            const color = obj.toHexString();
-            if (!hexCodes.includes(color)) {
-                hexCodes.push(color);
-            }
-        });
-        setAnalogous([...hexCodes]);
-    }
-
     return (
         <section className="accent-select">
             <h2>Accent select</h2>
-            <AccentColorScheme name="Analogous" colors={analogous} />
+            <AccentColorScheme scheme="Analogous" primary={primary} />
 
             <label htmlFor="accent-input">Or add a custom color:</label>
             <input
