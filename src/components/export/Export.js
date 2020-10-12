20cb43a824544ee5e731e266ae55c1b455def47b
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-12 15:22:39 -0700

    Added export format radio group

--- src/components/export/Export.js
+++ src/components/export/Export.js
@@ -8,31 +8,56 @@ export default function Export({
     primaryText,
     accentText,
 }) {
-    const [value, setValue] = useState("");
+    const [textareaValue, setTextareaValue] = useState("");
+    const [radioValue, setRadioValue] = useState("");
 
     useEffect(() => {
-        setValue(
+        setTextareaValue(
             `--primary: ${primary};\n` +
                 `--accent: ${accent};\n` +
                 `--body-text: ${bodyText};\n` +
                 `--background: ${background};\n` +
                 `--primary-text: ${primaryText};\n` +
-                `--accent-text: ${accentText};\n`
+                `--accent-text: ${accentText};`
         );
     }, [primary, accent, bodyText, background, primaryText, accentText]);
 
-    function handleChange(event) {
-        setValue(event.target.value);
+    function handleTextareaChange(event) {
+        setTextareaValue(event.target.value);
+    }
+
+    function handleRadioChange(event) {
+        setRadioValue(event.target.value);
     }
 
     return (
         <section>
             <h2>Export</h2>
 
+            <fieldset onChange={handleRadioChange}>
+                <legend>Select a format:</legend>
+                <label>
+                    <input type="radio" name="export-format" value="css" />
+                    CSS
+                </label>
+                <label>
+                    <input type="radio" name="export-format" value="scss" />
+                    SCSS
+                </label>
+                <label>
+                    <input type="radio" name="export-format" value="sass" />
+                    Sass
+                </label>
+                <label>
+                    <input type="radio" name="export-format" value="less" />
+                    Less
+                </label>
+            </fieldset>
+
             <textarea
-                value={value}
-                onChange={handleChange}
-                rows="5"
+                value={textareaValue}
+                onChange={handleTextareaChange}
+                rows="6"
                 cols="30"
             />
         </section>
