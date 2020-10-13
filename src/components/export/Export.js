716f7db8977e64fefc5dd577312cebf6dcaabf44
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-12 17:33:47 -0700

    Implemented functionality for export functions

--- src/components/export/Export.js
+++ src/components/export/Export.js
@@ -9,18 +9,46 @@ export default function Export({
     accentText,
 }) {
     const [textareaValue, setTextareaValue] = useState("");
-    const [radioValue, setRadioValue] = useState("");
+    const [radioValue, setRadioValue] = useState("css");
 
     useEffect(() => {
-        setTextareaValue(
-            `--primary: ${primary};\n` +
-                `--accent: ${accent};\n` +
-                `--body-text: ${bodyText};\n` +
-                `--background: ${background};\n` +
-                `--primary-text: ${primaryText};\n` +
-                `--accent-text: ${accentText};`
-        );
-    }, [primary, accent, bodyText, background, primaryText, accentText]);
+        if (radioValue === "scss") {
+            setTextareaValue(
+                `$primary: ${primary};\n` +
+                    `$accent: ${accent};\n` +
+                    `$body-text: ${bodyText};\n` +
+                    `$background: ${background};\n` +
+                    `$primary-text: ${primaryText};\n` +
+                    `$accent-text: ${accentText};`
+            );
+        } else if (radioValue === "less") {
+            setTextareaValue(
+                `@primary: ${primary};\n` +
+                    `@accent: ${accent};\n` +
+                    `@body-text: ${bodyText};\n` +
+                    `@background: ${background};\n` +
+                    `@primary-text: ${primaryText};\n` +
+                    `@accent-text: ${accentText};`
+            );
+        } else {
+            setTextareaValue(
+                `--primary: ${primary};\n` +
+                    `--accent: ${accent};\n` +
+                    `--body-text: ${bodyText};\n` +
+                    `--background: ${background};\n` +
+                    `--primary-text: ${primaryText};\n` +
+                    `--accent-text: ${accentText};`
+            );
+        }
+    }, [
+        primary,
+        accent,
+        bodyText,
+        background,
+        primaryText,
+        accentText,
+        radioValue,
+    ]);
 
     function handleTextareaChange(event) {
         setTextareaValue(event.target.value);
@@ -37,16 +65,17 @@ export default function Export({
             <fieldset onChange={handleRadioChange}>
                 <legend>Select a format:</legend>
                 <label>
-                    <input type="radio" name="export-format" value="css" />
+                    <input
+                        type="radio"
+                        name="export-format"
+                        value="css"
+                        defaultChecked
+                    />
                     CSS
                 </label>
                 <label>
                     <input type="radio" name="export-format" value="scss" />
                     SCSS
-                </label>
-                <label>
-                    <input type="radio" name="export-format" value="sass" />
-                    Sass
                 </label>
                 <label>
                     <input type="radio" name="export-format" value="less" />
