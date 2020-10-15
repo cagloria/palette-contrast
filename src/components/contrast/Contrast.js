9f2bc44230da7072b929899dec06915e07464991
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-14 17:49:55 -0700

    Added contrast information

--- src/components/contrast/Contrast.js
+++ src/components/contrast/Contrast.js
@@ -41,6 +41,31 @@ export default function Contrast({
         <section>
             <h2>Contrast check</h2>
 
+            <div className="contrast-information">
+                <p>
+                    Level AA requires a contrast ratio of 4.5:1 for normal text and
+                    3:1 for large text, and a contrast ratio of 3:1 for user
+                    interface components. Level AAA requires a contrast ratio of 7:1
+                    for normal text and 4.5:1 for large text.
+                </p>
+                <p>
+                    Large text is defined as being at least 18pt (24px) or 14pt
+                    (19px) bold. Read more about{" "}
+                    <a href="https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html">
+                        Level AA contrast
+                    </a>
+                    ,{" "}
+                    <a href="https://www.w3.org/WAI/WCAG21/Understanding/non-text-contrast">
+                        Level AA non-text contrast
+                    </a>
+                    , and{" "}
+                    <a href="https://www.w3.org/WAI/WCAG21/Understanding/contrast-enhanced">
+                        Level AAA contrast
+                    </a>
+                    .
+                </p>
+            </div>
+
             <LevelButton
                 aria-label={`Switch level to ${level === "AA" ? "AAA" : "AA"}`}
                 onClick={changeLevel}
