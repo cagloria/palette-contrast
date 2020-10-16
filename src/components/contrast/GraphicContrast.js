4ec2d61196d2b41130047d7cd55f1dd8852ad621
cagloria <33134245+cagloria@users.noreply.github.com>
2020-10-15 21:25:17 -0700

    Added styling to primary and accent contrast panel

--- src/components/contrast/GraphicContrast.js
+++ src/components/contrast/GraphicContrast.js
@@ -2,31 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import ContrastTest from "./ContrastTest";
 
-const Panel1 = styled.div`
+const Color1 = styled.div`
     background-color: ${(props) => props.color};
 `;
 
-const Panel2 = styled.div`
+const Color2 = styled.div`
     background-color: ${(props) => props.color};
 `;
 
 export default function GraphicContrast({ color1, color2 }) {
     return (
-        <div>
+        <div className="graphic-contrast component-panel">
             <h3>Primary and accent</h3>
-            <div>
-                {/* Graphics/UI component contrast ratio is defined only in 
-                    level AA as 3:1, the same as large text */}
-                <ContrastTest
-                    level="AA"
-                    color1={color1}
-                    color2={color2}
-                    size="large"
-                />
-                <div className="graphic-panel-container">
-                    <Panel1 className="graphic-panel-1" color={color1} />
-                    <Panel2 className="graphic-panel-2" color={color2} />
-                </div>
+            
+            {/* Graphics/UI component contrast ratio is defined only in level AA
+                as 3:1, the same as large text */}
+            <ContrastTest
+                level="AA"
+                color1={color1}
+                color2={color2}
+                size="large"
+            />
+
+            <div className="graphic-contrast__colors-container">
+                <Color1 className="graphic-contrast__color-1" color={color1} />
+                <Color2 className="graphic-contrast__color-2" color={color2} />
             </div>
         </div>
     );
